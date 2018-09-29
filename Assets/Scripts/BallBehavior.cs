e03a83e3920e78bd62ff84df7a1f08950322e2bc
WLLMGS <willem.goos@hotmail.com>
2018-09-29 16:51:58 +0200

    added skybox assets, added custom plane for direction, added plane scale with direction

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -14,13 +14,19 @@ public class BallBehavior : MonoBehaviour
 
     private Vector3 _dir = new Vector3();
 
+    private GameObject _directionArrow;
+    private GameObject _directionPlane;
     // Use this for initialization
     void Start()
     {
         _rigid = GetComponent<Rigidbody>();
+        _directionArrow = transform.Find("directionArrow").gameObject;
+        _directionPlane = _directionArrow.transform.Find("plane").gameObject;
 
 #if DEBUG
         Assert.IsNotNull(_rigid, "No rigid body on player ball object");
+        Assert.IsNotNull(_directionArrow, "No direction arrow on player ball object");
+        Assert.IsNotNull(_directionPlane, "No direction plane on player ball object");
 #endif
     }
 
@@ -34,16 +40,32 @@ public class BallBehavior : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            if(!_directionArrow.activeSelf) _directionArrow.SetActive(true);
+
+
             float xm = Input.GetAxis("Mouse X");
             float ym = Input.GetAxis("Mouse Y");
             //Debug.Log("MOUSE MOVEMENT: [" + xm + "," + ym + "]");
 
             _dir += new Vector3(xm, 0, ym);
             _rigid.velocity = Vector3.zero;
+
+
+            float angle = Mathf.Atan2(_dir.x, _dir.z);
+            float camAngle = Camera.main.transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+            angle += camAngle + Mathf.PI / 2.0f;
+
+           
+            _directionArrow.transform.rotation = Quaternion.Euler(new Vector3(0,angle * Mathf.Rad2Deg, 0));
+
+           _directionPlane.transform.localScale = new Vector3(_dir.magnitude, 0.1f, 0.05f);
+
         }
 
         if (Input.GetMouseButtonUp(0))
         {
+            _directionArrow.SetActive(false);
+            
             float angle = Mathf.Atan2(_dir.x, _dir.z);
             float camAngle = Camera.main.transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
 
