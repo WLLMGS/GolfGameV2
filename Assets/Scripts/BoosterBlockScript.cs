225cf2d2581ddab5f496e1da6ff9e4ae5c9029b3
WLLMGS <willem.goos@hotmail.com>
2018-11-30 14:33:12 +0100

    finished 3rd hole

---
+++ Assets/Scripts/BoosterBlockScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterBlockScript : MonoBehaviour {
+
+
+	[SerializeField] private float _multiplier = 100.0f;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Rigidbody rb = other.GetComponent<Rigidbody>();
+			if(rb == null) return;
+
+			//boost player when entered
+			Vector3 currVelocity = rb.velocity;
+			currVelocity.Normalize();
+			currVelocity *= _multiplier;
+
+			rb.velocity = currVelocity;
+		}
+	}
+}
